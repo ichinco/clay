530f7f2c9a32c49228dba865f4961fcc0ab33248
Denise Ichinco <ichinco@gmail.com>
2011-11-09 22:48:14 -0500

    image serverice

---
+++ src/grails-app/services/com/clay/ImageService.groovy
@@ -0,0 +1,43 @@
+package com.clay
+
+class ImageService {
+
+    static transactional = true
+
+    def addImagePoint(ImagePoint point, int imageId) {
+        Image image = Image.get(imageId)
+        image.points.add(point)
+        image.save()
+
+        if (!image.validate()){
+            // TODO record errors
+        }
+    }
+
+    def createImagePoint(int x, int y, Product product){
+        ImagePoint imagePoint = new ImagePoint()
+        imagePoint.x = x
+        imagePoint.y = y
+        imagePoint.product = product
+        imagePoint.save()
+
+        if (!image.validate()){
+            // TODO record errors
+        }
+
+        return imagePoint
+    }
+
+    def createProduct(String name, String url){
+        Product product = new Product()
+        product.name = name
+        product.url = url
+        product.save()
+
+        if (!product.validate()){
+            // TODO record errors
+        }
+
+        return product
+    }
+}
