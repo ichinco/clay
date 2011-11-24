65ccd1caaa9a15763ac4e3f6d88c70f5bc4cd8fa
Denise Ichinco <ichinco@gmail.com>
2011-11-24 13:12:13 -0500

    allow users to change their votes

--- src/grails-app/services/com/clay/VoteService.groovy
+++ src/grails-app/services/com/clay/VoteService.groovy
@@ -10,40 +10,52 @@ class VoteService {
     private VoteType down;
 
     def commentUp(User user, Comment comment) {
-        CommentVote vote = new CommentVote()
-        vote.user = user
+        CommentVote vote = CommentVote.findByUserAndComment(user,comment)
+        if (!vote){
+            vote = new CommentVote()
+            vote.user = user
+            vote.comment = comment
+        }
         vote.type = up
-        vote.comment = comment
         vote.save()
 
         return vote
     }
 
     def commentDown(User user, Comment comment) {
-        CommentVote vote = new CommentVote()
-        vote.user = user
+        CommentVote vote = CommentVote.findByUserAndComment(user,comment)
+        if (!vote){
+            vote = new CommentVote()
+            vote.user = user
+            vote.comment = comment
+        }
         vote.type = down
-        vote.comment = comment
         vote.save()
 
         return vote
     }
 
     def designUp(User user, Design design){
-        DesignVote vote = new DesignVote()
-        vote.user = user
+        DesignVote vote = DesignVote.findByUserAndDesign(user,design)
+        if (!vote){
+            vote = new DesignVote()
+            vote.user = user
+            vote.design = design
+        }
         vote.type = up
-        vote.design = design
         vote.save()
 
         return vote
     }
 
     def designDown(User user, Design design){
-        DesignVote vote = new DesignVote()
-        vote.user = user
+        DesignVote vote = DesignVote.findByUserAndDesign(user,design)
+        if (!vote){
+            vote = new DesignVote()
+            vote.user = user
+            vote.design = design
+        }
         vote.type = up
-        vote.design = design
         vote.save()
 
         return vote
